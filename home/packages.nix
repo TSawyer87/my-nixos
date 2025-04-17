6d03c27c50bf2964c9baa72d268c4d3f6b6fc31f
TSawyer87 <sawyerjr.25@gmail.com>
2025-04-17 14:51:54 -0400

    fmt

--- home/packages.nix
+++ home/packages.nix
@@ -3,7 +3,8 @@
   host,
   userVars,
   ...
-}: {
+}:
+{
   home.packages = [
     # inputs.zen-browser.packages."${pkgs.system}".default
     pkgs.oh-my-zsh
@@ -15,16 +16,16 @@
     pkgs.usbutils # lsusb
     pkgs.nitch
     pkgs.nix-fast-build
-    (import ../scripts/emopicker9000.nix {inherit pkgs;})
-    (import ../scripts/task-waybar.nix {inherit pkgs;})
-    (import ../scripts/squirtle.nix {inherit pkgs;})
+    (import ../scripts/emopicker9000.nix { inherit pkgs; })
+    (import ../scripts/task-waybar.nix { inherit pkgs; })
+    (import ../scripts/squirtle.nix { inherit pkgs; })
     (import ../scripts/wallsetter.nix {
       inherit pkgs;
       inherit userVars;
     })
-    (import ../scripts/web-search.nix {inherit pkgs;})
-    (import ../scripts/rofi-launcher.nix {inherit pkgs;})
-    (import ../scripts/screenshootin.nix {inherit pkgs;})
+    (import ../scripts/web-search.nix { inherit pkgs; })
+    (import ../scripts/rofi-launcher.nix { inherit pkgs; })
+    (import ../scripts/screenshootin.nix { inherit pkgs; })
     (import ../scripts/list-hypr-bindings.nix {
       inherit pkgs;
       inherit host;
