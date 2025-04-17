2c6948295d6220d6d9f90c34633861d5a4fc7474
TSawyer87 <sawyerjr.25@gmail.com>
2025-04-17 14:07:44 -0400

    nushell alias

--- home/shells/nushell/default.nix
+++ home/shells/nushell/default.nix
@@ -52,9 +52,9 @@
 
           # Nix
           # fr = "nh os switch --hostname magic /home/jr/flakes";
-          fr = "nh os switch /home/jr/" + userVars.flake;
-          ft = "nh os test --hostname magic /home/jr/" + userVars.flake;
-          fu = "nh os switch --hostname magic --update /home/jr/" + userVars.flake;
+          fr = "nh os switch" + userVars.flake;
+          ft = "nh os test --hostname magic" + userVars.flake;
+          fu = "nh os switch --hostname magic --update" + userVars.flake;
           opts = "man home-configuration.nix";
 
           cat = "${pkgs.bat}/bin/bat";
