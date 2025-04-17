c0c13e2d8a5492585827933c64b06d299b364a04
TSawyer87 <sawyerjr.25@gmail.com>
2025-04-17 15:48:58 -0400

    treefmt formatter settings

--- treefmt.nix
+++ treefmt.nix
@@ -2,20 +2,54 @@
   projectRootFile = "flake.nix";
   programs = {
     deadnix.enable = true;
+    rustfmt.enable = true;
+    shellcheck.enable = true;
+    prettier.enable = true;
+    statix.enable = true;
     keep-sorted.enable = true;
     nixfmt = {
       enable = true;
-      strict = true;
+      # strict = true;
     };
   };
-  settings.excludes = [
-    "*.age"
-    "*.jpg"
-    "*.nu"
-    "*.png"
-    ".jj/*"
-    "flake.lock"
-    "hive/moonrise/borg-key-backup"
-    "justfile"
-  ];
+  settings = {
+    global.excludes = [
+      "LICENSE"
+      ".adr-dir"
+      # unsupported extensions
+      "*.{gif,png,svg,tape,mts,lock,mod,sum,toml,env,envrc,gitignore,sql,conf,pem,*.so.2,key,pub,py,narHash}"
+      "data-mesher/test/networks/*"
+      "nss-datamesher/test/dns.json"
+      "*.age"
+      "*.jpg"
+      "*.nu"
+      "*.png"
+      ".jj/*"
+      "flake.lock"
+      "hive/moonrise/borg-key-backup"
+      "justfile"
+    ];
+
+    formatter = {
+      deadnix = {
+        priority = 1;
+      };
+
+      statix = {
+        priority = 2;
+      };
+
+      nixfmt = {
+        priority = 3;
+      };
+
+      prettier = {
+        options = [
+          "--tab-width"
+          "4"
+        ];
+        includes = ["*.{css,html,js,json,jsx,md,mdx,scss,ts,yaml}"];
+      };
+    };
+  };
 }
