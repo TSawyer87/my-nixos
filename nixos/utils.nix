7c5983ce65ffc4875c556a0587b686015af21e75
TSawyer87 <sawyerjr.25@gmail.com>
2025-04-18 08:30:34 -0400

    sqlite

--- nixos/utils.nix
+++ nixos/utils.nix
@@ -2,11 +2,11 @@
   pkgs,
   # inputs,
   ...
-}:
-{
+}: {
   environment.systemPackages = with pkgs; [
     # inputs.nix-inspect.packages.${pkgs.system}.default # util for inspecting derivations
     cheat # Display cheat sheets for commands
+    sqlite
     inxi # show detailed system info
     wget
     eza
