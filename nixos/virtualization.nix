a76cc4994134244f1cdecd7c18d7714a1fddccb1
TSawyer87 <sawyerjr.25@gmail.com>
2025-04-21 09:21:45 -0400

    qemu

--- nixos/virtualization.nix
+++ nixos/virtualization.nix
@@ -6,8 +6,8 @@
 }: {
   environment.systemPackages = [pkgs.qemu_kvm];
   virtualisation.libvirtd.enable = true;
-  virtualisation.qemu.package = pkgs.qemu_kvm;
-  virtualisation.qemu.options = ["-enable-kvm"];
+  # virtualisation.qemu.package = pkgs.qemu_kvm;
+  # virtualisation.qemu.options = ["-enable-kvm"];
   virtualisation.vmVariant = "kvm"; # Optimized for KVM
   virtualisation.podman = {
     enable = false;
