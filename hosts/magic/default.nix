d1bcba90f1eb7fcdc0174e53b0b37fdbeddc416f
TSawyer87 <sawyerjr.25@gmail.com>
2025-04-22 17:44:23 -0400

    userVars

--- hosts/magic/default.nix
+++ hosts/magic/default.nix
@@ -1,6 +1,6 @@
 {inputs, ...}:
 inputs.nixpkgs.lib.nixosSystem {
-  inherit (inputs.lib) system host userVars;
+  inherit (inputs.lib) system userVars;
   specialArgs = {inherit inputs;};
   modules = [./configuration.nix];
 }
