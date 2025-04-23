80e76b56dd871218f475dc675f373d6b7aa705a1
TSawyer87 <sawyerjr.25@gmail.com>
2025-04-23 09:51:13 -0400

    refactor pac_defs

--- lib/overlay.nix
+++ lib/overlay.nix
@@ -5,7 +5,7 @@ _final: prev: let
   # Define all packages
   packages = {
     # Additional packages
-    pokego = callPackage ./pokego.nix {};
+    pokego = callPackage ./pac_defs/pokego.nix {};
     pokemon-colorscripts = callPackage ./pac_defs/pokemon-colorscripts.nix {};
     python-pyamdgpuinfo = callPackage ./pac_defs/python-pyamdgpuinfo.nix {};
     Tela-circle-dracula = callPackage ./pac_defs/Tela-circle-dracula.nix {};
